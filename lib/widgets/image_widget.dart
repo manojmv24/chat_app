705d2037c0a7f231f079466c0a7e336afd4ed5db
manojmv24 <manojmv24@gmail.com>
2023-10-02 17:02:09 +0530

    pick image from camera has been added

--- lib/widgets/image_widget.dart
+++ lib/widgets/image_widget.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageWidget extends StatefulWidget {
   const ImageWidget({Key? key}) : super(key: key);
@@ -8,20 +11,35 @@ class ImageWidget extends StatefulWidget {
 }
 
 class _ImageWidgetState extends State<ImageWidget> {
+  File? pickedImageFile;
+  void pickImage() async {
+    var pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.camera, maxWidth: 150, imageQuality: 50);
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      pickedImageFile = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        const CircleAvatar(
+        CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
+          foregroundImage: pickedImageFile == null ? null : FileImage(pickedImageFile!),
         ),
         const SizedBox(
           height: 20,
         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: () {
+            pickImage();
+          },
           icon: const Icon(Icons.upload),
           label: const Text("Upload Image"),
         )
